9aa63a386a9d630cc4ddc54acd5ce0dad9cb2ec4
Laura  Watkin <laura.watkin@mail.mcgill.ca>
2020-02-08 16:18:46 -0500

    fixed k-cross

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def evaluate_acc(trueLabels, predictLabels):
 def k_cross_validation(trainningData, model):
 	#need to split trainning data into X and y somehow
 	accuracies = []
-	k_folds = split_data(trainningData)
+	k_folds, y_folds = split_data(trainningData)
 	for i in range(5):
 		if(model=="nb"):
 			nbModel=NaiveBayes() 
-			nbModel.fit(k_folds[i], y)
+			nbModel.fit(k_folds[i], y_folds[i])
 			testSet = k_folds[:i]+k_folds[i+1:]
 			predictions = nbModel.predict(testSet)
 			test_y = predictions[-1]
@@ -29,10 +29,11 @@ def k_cross_validation(trainningData, model):
 
 		else:
 			logModel=LogRegression() 
-			w = logModel.fit(k_folds[i], y, 0.001, 0.001)
+			w = logModel.fit(k_folds[i], y_folds[i], 0.001, 0.001)
 			testSet = k_folds[:i]+k_folds[i+1:]
 			predictions = logModel.predict(testSet, w)
-			accuracies.append(evaluate_acc(y, predictions))
+			test_y = predictions[-1]
+			accuracies.append(evaluate_acc(test_y, predictions))
 			
 	avg_accuracy = sum(accuracies)/5
 	return avg_accuracy
@@ -40,6 +41,7 @@ def k_cross_validation(trainningData, model):
 
 def split_data(X):
 	split_data = []
+	split_y=[]
 	copy_data = X
 	size_folds = int(len(X)/5)
 
@@ -49,4 +51,5 @@ def split_data(X):
 			i = random.randrange(len(copy_data))
 			fold.append(copy_data[i])
 		split_data.append(fold)
-	return np.array(split_data)
+		split_y.append(fold[:,-1])
+	return np.array(split_data), np.array(split_y)
