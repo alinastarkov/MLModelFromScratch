34ad1b4a0c9ade800fb85b8570df759a388e56c3
alinastarkov <hayleeluu@gmail.com>
2020-02-08 23:14:02 -0500

    run dataset

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from naivesbayes import NaiveBayes
 from sklearn.utils import shuffle
 from logregression import LogRegression 
 import random
+
+#for testing purpose
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
 
+
+#import clean data 
+from ionosphere_cleaning import X_ionosphere, y_ionosphere
+from HabCleaning import X_haberman, y_haberman
+from cleanAdult import X_adult, y_adult
+from abaloneCleaning import X_abalone, y_abalone
 
 #evaluate the model accuracy
 #function takes the true label and target label as input, and output the accuracy score 
@@ -30,11 +39,15 @@ def k_cross_validation(trainningData, label, model):
 			np.delete(Ytrain, i, 0)
 			Xtrain = np.concatenate(Xtrain)
 			Ytrain = np.concatenate(Ytrain)
-
+			
 			nbModel=NaiveBayes() 
 			nbModel.fit(Xtrain, Ytrain)
 			predictions = nbModel.predict(testSet)
+			# nbModel = GaussianNB()
+			# nbModel.fit(Xtrain, Ytrain)
+			# predictions = nbModel.predict(testSet)
 			accuracies.append(evaluate_acc(test_y, predictions))
+
 		else:
 			Xtrain = k_folds[0].copy()
 			Ytrain = k_folds[1].copy()
@@ -50,25 +63,26 @@ def k_cross_validation(trainningData, label, model):
 			predictions = logModel.predict(testSet, w)
 			accuracies.append(evaluate_acc(test_y, predictions))
 
-
 	avg_accuracy = sum(accuracies)/5
 	return avg_accuracy
 
 
 def split_data(X, y):
 	return np.array_split(X, 5), np.array_split(y, 5)
-
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-# nbModel=NaiveBayes() 
-# nbModel.fit(X_train, y_train)
-# predictions = nbModel.predict(X_test)
-# a = evaluate_acc(y_test, predictions)
-# print(a)
-
-bc = datasets.load_iris()
-X, y = bc.data, bc.target
-test = k_cross_validation(X, y, "nb")
-testlog = k_cross_validation(X, y, "log")
+i
+print("haberman")
+test = k_cross_validation(X_haberman, y_haberman, "nb")
+testlog = k_cross_validation(X_haberman, y_haberman, "log")
 print(test)
 print(testlog)
+
+# print("Adult")
+# test = k_cross_validation(X_adult, y_adult, "nb")
+# testlog = k_cross_validation(X_adult, y_adult, "log")
+
+print("Ionosphere")
+test = k_cross_validation(X_ionosphere, y_ionosphere, "nb")
+testlog = k_cross_validation(X_ionosphere, y_ionosphere, "log")
+print(test)
+print(testlog)
+
