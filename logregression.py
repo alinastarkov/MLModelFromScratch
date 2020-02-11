436567db80dfb64ed695596163552bb4b16b8a6b
dylancaverly <60328343+dylancaverly@users.noreply.github.com>
2020-02-11 17:54:56 -0500

    removed n_iter

--- logregression.py
+++ logregression.py
@@ -28,7 +28,7 @@ class LogRegression:
         while np.linalg.norm(g) > self.eps:
             g = self._gradient(X, y, w)
             w = w - self.lr*(g)
-            return w, n_iter;
+            return w
 
     def predict(self,X,w):
         yh = self._logistic(np.dot(X, w))
