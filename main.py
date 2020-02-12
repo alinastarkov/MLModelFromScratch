1aac0704fb2083673f638c995a45e660f2ec5938
alinastarkov <hayleeluu@gmail.com>
2020-02-11 21:46:56 -0500

    one last check

--- main.py
+++ main.py
@@ -104,129 +104,152 @@ def validation_logistic(Xtrain, Xtest, y_train, y_test):
 	return (sum(accuracies)/5)
 
 # --------EXPERIMENT 2----------
-# print("haberman")
-# X_train, X_test, y_train, y_test = train_test_split(X_haberman_cont, y_haberman, test_size=0.10, random_state=42)
-# X_traincat, X_testcat, y_traincat, y_testcat = train_test_split(X_haberman_cat, y_haberman, test_size=0.10, random_state=42)
-# Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_haberman, y_haberman, test_size=0.10, random_state=42)
+print("EXPERIMENT 2")
+print("haberman")
+X_train, X_test, y_train, y_test = train_test_split(X_haberman_cont, y_haberman, test_size=0.10, random_state=42)
+X_traincat, X_testcat, y_traincat, y_testcat = train_test_split(X_haberman_cat, y_haberman, test_size=0.10, random_state=42)
+Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_haberman, y_haberman, test_size=0.10, random_state=42)
 
-# print("naive bayes training ")
-# test = k_cross_validation(X_train, yh_train, "nb", 1, X_traincat)
-# print(test)
-# print("Logistic regression training")
-# testlog = k_cross_validation(Xh_train, yh_train, "log", 0)
-# print(testlog)
+print("naive bayes training ")
+test = k_cross_validation(X_train, yh_train, "nb", 1, X_traincat)[0]
+print(test)
+print("Logistic regression training")
+testlog = k_cross_validation(Xh_train, yh_train, "log", 0)[0]
+print(testlog)
 
-# print("Adult")
-# X_train, X_test, y_train, y_test = train_test_split(X_adult_cont, y_adult, test_size=0.10, random_state=42)
-# X_traincat, X_testcat, y_traincat, y_testcat = train_test_split(X_adult_cat, y_adult, test_size=0.10, random_state=42)
-# Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_adult, y_adult, test_size=0.10, random_state=42)
-# print("naive bayes training ")
-# test = k_cross_validation(X_train, yh_train, "nb", 1, X_traincat)
-# print(test)
-# print("Logistic regression training")
-# testlog = k_cross_validation(Xh_train, yh_train, "log", 0)
-# print(testlog)
+print("Adult")
+X_train, X_test, y_train, y_test = train_test_split(X_adult_cont, y_adult, test_size=0.10, random_state=42)
+X_traincat, X_testcat, y_traincat, y_testcat = train_test_split(X_adult_cat, y_adult, test_size=0.10, random_state=42)
+Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_adult, y_adult, test_size=0.10, random_state=42)
+print("naive bayes training ")
+test = k_cross_validation(X_train, yh_train, "nb", 1, X_traincat)[0]
+print(test)
+print("Logistic regression training")
+testlog = k_cross_validation(Xh_train, yh_train, "log", 0)[0]
+print(testlog)
 
-# ------EXPERIMENT 3---------
-# ~~~~~~NAIVES BAYES~~~~~~
-# split x and y into 90-10 training and testing set 
-# X_train, X_test, y_train, y_test = train_test_split(X_haberman_cont, y_haberman, test_size=0.10, random_state=42)
-# X_traincat, X_testcat, y_traincat, y_testcat = train_test_split(X_haberman_cat, y_haberman, test_size=0.10, random_state=42)
+print("Iris")
+X_train, X_test, y_train, y_test = train_test_split(X_iris, y_iris, test_size=0.10, random_state=42)
+Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_train, y_train, test_size=0.10, random_state=42)
+print("naive bayes training ")
+test = k_cross_validation(X_train, y_train, "nb", 0)[0]
+print(test)
+print("Logistic regression training")
+testlog = k_cross_validation(X_train, y_train, "log", 0)[0]
+print(testlog)
 
-# nbModel= NaiveBayes(1) # if be.fit(1)
-# accuracies = []
-# for _ in range(5):
-# 	nbModel.fit(X_train, y_train, X_traincat)
-# 	predictions = nbModel.predict(X_test,X_testcat)
-# 	pred = evaluate_acc(y_test, predictions)
-# 	accuracies.append(pred)
+print("Ionosphere")
+X_train, X_test, y_train, y_test = train_test_split(X_ionosphere, y_ionosphere, test_size=0.10, random_state=42)
+Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_train, y_train, test_size=0.10, random_state=42)
+print("naive bayes training ")
+test = k_cross_validation(X_train, y_train, "nb", 0)[0]
+print(test)
+print("Logistic regression training")
+testlog = k_cross_validation(X_train, y_train, "log", 0)[0]
+print(testlog)
 
-# print("test size = 100%")
-# print("Validation accuracy")
-# print(sum(accuracies)/5)
-# print("training accuracy")
-# test = k_cross_validation(X_train, y_train, "nb", 0, X_traincat)
-# print(test)
 
-# test_size = [0.15, 0.30, 0.45] 
 
-# for size in test_size:
-# 	control training size for the experiment 
-# 	X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train, y_train, test_size=size, random_state=5)
-# 	X_traincat1, X_testcat1, y_traincat1, y_testcat1 = train_test_split(X_traincat, y_traincat, test_size=size, random_state=5)
-# 	accuracies = []
+#------EXPERIMENT 3---------
+#~~~~~~NAIVES BAYES~~~~~~
+#split x and y into 90-10 training and testing set 
 
-# 	for _ in range(5):
-# 		nbModel= NaiveBayes(1) # if be.fit(1)
-# 		nbModel.fit(X_train1, y_train1, X_traincat1)
-# 		predictions = nbModel.predict(X_test,X_testcat)
-# 		pred = evaluate_acc(y_test, predictions)
-# 		accuracies.append(pred)
-# 	print("test size " + str(size))
-# 	print("Validation accuracy")
-# 	print(sum(accuracies)/5)
-# 	print("Training accuracy")
-# 	test = k_cross_validation(X_train1, y_train1, "nb", 0,X_traincat1 )
-# 	print(test)
+
+print("EXPERIMENT 2- NAIVE BAYES")
+X_train, X_test, y_train, y_test = train_test_split(X_haberman_cont, y_haberman, test_size=0.10, random_state=42)
+X_traincat, X_testcat, y_traincat, y_testcat = train_test_split(X_haberman_cat, y_haberman, test_size=0.10, random_state=42)
+
+nbModel= NaiveBayes(1) # if be.fit(1)
+accuracies = []
+for _ in range(5):
+	nbModel.fit(X_train, y_train, X_traincat)
+	predictions = nbModel.predict(X_test,X_testcat)
+	pred = evaluate_acc(y_test, predictions)
+	accuracies.append(pred)
+
+print("test size = 100%")
+print("Validation accuracy")
+print(sum(accuracies)/5)
+print("training accuracy")
+test = k_cross_validation(X_train, y_train, "nb", 0, X_traincat)[0]
+print(test)
+
+test_size = [0.15, 0.30, 0.45] 
+
+for size in test_size:
+	X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train, y_train, test_size=size, random_state=5)
+	X_traincat1, X_testcat1, y_traincat1, y_testcat1 = train_test_split(X_traincat, y_traincat, test_size=size, random_state=5)
+	accuracies = []
+
+	for _ in range(5):
+		nbModel= NaiveBayes(1) # if be.fit(1)
+		nbModel.fit(X_train1, y_train1, X_traincat1)
+		predictions = nbModel.predict(X_test,X_testcat)
+		pred = evaluate_acc(y_test, predictions)
+		accuracies.append(pred)
+	print("test size " + str(size))
+	print("Validation accuracy")
+	print(sum(accuracies)/5)
+	print("Training accuracy")
+	test = k_cross_validation(X_train1, y_train1, "nb", 0,X_traincat1 )[0]
+	print(test)
+
 
 # ~~~~~LOGISTIC REGRESSION ~~~~~~
 
-# from ionosphere_cleaning import X_ionosphere, y_ionosphere
-# from HabCleaning import X_haberman_cat, X_haberman_cont, y_haberman, X_haberman
-# from cleanAdult import X_adult_cat, X_adult_cont, y_adult, X_adult
-# from cleanIris import X_iris, y_iris
 
-# X_train, X_test, y_train, test_y = train_test_split(X_haberman, y_haberman, test_size=0.10)
-# accuracies =[]
-# for _ in range(5):
-# 	logModel=LogRegression(0.001, 0.01) 
-# 	w = logModel.fit(X_train, y_train)
-# 	predictions = logModel.predict(X_test, w)
-# 	accuracies.append(evaluate_acc(test_y, predictions))
+print("EXPERIMENT 2- LOGISTIC REGRESSION")
+X_train, X_test, y_train, test_y = train_test_split(X_haberman, y_haberman, test_size=0.10)
+accuracies =[]
+for _ in range(5):
+	logModel=LogRegression(0.001, 0.01) 
+	w = logModel.fit(X_train, y_train)
+	predictions = logModel.predict(X_test, w)
+	accuracies.append(evaluate_acc(test_y, predictions))
 
-# print("test size = 100%")
-# print("Validation accuracy")
-# print(sum(accuracies)/5)
-# print("training accuracy")
-# test = k_cross_validation(X_train, y_train, "log", 0)
-# print(test)
+print("test size = 100%")
+print("Validation accuracy")
+print(sum(accuracies)/5)
+print("training accuracy")
+test = k_cross_validation(X_train, y_train, "log", 0)[0]
+print(test)
 
-# test_size = [0.15, 0.30, 0.45] 
+test_size = [0.15, 0.30, 0.45] 
 
-# for size in test_size:
-# 	control training size for the experiment 
-# 	X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train, y_train, test_size=size)
-# 	accuracies = []
+for size in test_size:
+	X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train, y_train, test_size=size)
+	accuracies = []
 
-# 	for _ in range(5):
-# 		X_train, X_test, y_train, test_y = train_test_split(X_iris, y_iris, test_size=0.10)
-# 		logModel=LogRegression(0.001, 0.01) 
-# 		w = logModel.fit(X_train, y_train)
-# 		predictions = logModel.predict(X_test, w)
-# 		accuracies.append(evaluate_acc(test_y, predictions))
-# 	print("test size " + str(size))
-# 	print("Validation accuracy")
-# 	print(sum(accuracies)/5)
-# 	print("Training accuracy")
-# 	test = k_cross_validation(X_train1, y_train1, "log", 0 )
-# 	print(test)
+	for _ in range(5):
+		X_train, X_test, y_train, test_y = train_test_split(X_iris, y_iris, test_size=0.10)
+		logModel=LogRegression(0.001, 0.01) 
+		w = logModel.fit(X_train, y_train)
+		predictions = logModel.predict(X_test, w)
+		accuracies.append(evaluate_acc(test_y, predictions))
+	print("test size " + str(size))
+	print("Validation accuracy")
+	print(sum(accuracies)/5)
+	print("Training accuracy")
+	test = k_cross_validation(X_train1, y_train1, "log", 0 )[0]
+	print(test)
 
 # ------------- EXPERIMENT 1 -------------
-# print("Ionosphere")
-# Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_ionosphere, y_ionosphere, test_size=0.10, random_state=42)
+print("EXPERIMENT 1")
+print("Ionosphere")
+Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_ionosphere, y_ionosphere, test_size=0.10, random_state=42)
 
-# print("naive bayes training ")
-# test = k_cross_validation(Xh_train, yh_train, "nb", 0)
-# print(test)
-# print("Logistic regression training")
-# testlog = k_cross_validation(Xh_train, yh_train, "log", 0)
-# print(testlog)
+print("naive bayes training ")
+test = k_cross_validation(Xh_train, yh_train, "nb", 0)
+print(test)
+print("Logistic regression training")
+testlog = k_cross_validation(Xh_train, yh_train, "log", 0)
+print(testlog)
 
-# print("Iris")
-# Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_iris, y_iris, test_size=0.10, random_state=42)
-# print("naive bayes")
-# test, n_iter = k_cross_validation(X_iris, y_iris, "nb", 0)
-# print(test)
-# print("Logistic regression")
-# testlog, n_iter = k_cross_validation(X_iris, y_iris, "log", 0)
-# print(testlog)
+print("Iris")
+Xh_train, Xh_test, yh_train, yh_test = train_test_split(X_iris, y_iris, test_size=0.10, random_state=42)
+print("naive bayes")
+test, n_iter = k_cross_validation(X_iris, y_iris, "nb", 0)
+print(test)
+print("Logistic regression")
+testlog, n_iter = k_cross_validation(X_iris, y_iris, "log", 0)
+print(testlog)
